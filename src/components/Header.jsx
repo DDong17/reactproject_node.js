ecf7a584a990e715488ac294b31b136748e795bb
DongHo Hwang <ghkdehdgh11@gmail.com>
2022-12-28 17:38:54 +0900

    header 작업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 export const Header = () => {
     return (
         <nav id="header" className="bg-black text-white">
-            <div className="w-full container mx-auto flex flex-wrap items center justify-content-between mt-0 py-2">
+            <div className="w-full container mx-auto flex flex-wrap items center justify-between mt-0 py-2">
                 <div className="logo-wrapper pl-4 flex items-center">
                     <Link to='/' className="toggleColore text-white no-underline hover:no-underlint font-bold text-2xl lg:text-4xl">
                     <img src={foody} alt="logo" className="w-40 h-40 object-cover" />
