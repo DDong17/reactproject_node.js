455ab2e0fb54299f5650dc2aca10aa43e7aa9bd4
DongHo Hwang <ghkdehdgh11@gmail.com>
2023-01-09 18:18:15 +0900

    ProductsPreview추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { Banner } from "../../components/Banner";
+import { ProductsPreview } from "../../components/ProductsPreview";
 import { About } from "../../components/About";
 const Home = () =>{
     return (
         <>
         <Banner/>
+        <ProductsPreview/>
         <About/>
         </>
         
